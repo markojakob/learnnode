765037cc6737c694d8d57c2ecfc6f387142f06f4
Mark Jakob Pontus <markjakobo@gmail.com>
2024-12-16 12:53:48 +0200

    Revert "fixed surge"
    This reverts commit 49241b5f17afc87552262dc29b628e6b0494fef4.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-export default async () => {
-  let character;
-  const response = await fetch('https://rickandmortyapi.com/api/character/17');
-  character = await response.json();
 
+export default async () => {
+  let response = await fetch('https://rickandmortyapi.com/api/character?page=17');
+  let body = await response.json();
+  let characters = body.results;
+  let pages = []
+  characters.forEach(character => {
+    let page = new HtmlWebpackPlugin({
+      template: './src/character.njk',
+      filename: "character_" + character.id + ".html",
+      templateParameters: {
+        character
+      },
+    });
+    pages.push(page);
+  });
+  console.log(body);
 
   return {
     entry: './src/index.js',
@@ -24,41 +36,47 @@ export default async () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.scss$/i,
+
           use: [
-            'style-loader',
-            'css-loader',
+            "style-loader", "css-loader",
             {
-              loader: 'sass-loader',
+              loader: "sass-loader",
               options: {
                 sassOptions: {
-                  quietDeps: true,
-                },
-              },
-            },
-          ],
+                  quietDeps: true
+                }
+              }
+            }
+          ]
         },
         {
           test: /\.njk$/,
           use: [
             {
               loader: 'simple-nunjucks-loader',
-              options: {},
-            },
-          ],
-        },
+              options: {}
+            }
+          ]
+        }
       ],
     },
     plugins: [
       new HtmlWebpackPlugin({
         template: './src/index.njk',
         templateParameters: {
-          character, // Andmed API-st
-        },
+          name: 'Mark',
+          characters, //characters: characters
+        }
       }),
+      new HtmlWebpackPlugin({
+        filename: 'about.html',
+        template: './src/about.njk'
+      }),
+      ...pages
     ],
   };
 };
