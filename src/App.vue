3387c78922c7a384ba610959f2da7a95d7856b56
Mark Jakob Pontus <markjakobo@gmail.com>
2025-01-14 11:39:15 +0200

    Add vue(kodus lõpetada)

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+import { ref } from 'vue';
+
+let message = ref('');
+let = ref(['Piim', 'Laua viin', 'Õlu', 'Krõpsud']);
+
+function addItem(){
+    if(message.value !== ''){
+        items.value.push(message.value);
+    }
+    message.value = "";
+}
+
+</script>
+
+<template>
+    <div class="container mt-2">
+        <div class="field has-addons">
+                <div class="control">
+                    <input class="input" v-model="message" @keypress.enter="addItem">
+                </div>
+                <div class="control">
+                    <button class="button is-info" @click="addItem">
+                        Search
+                    </button>
+                </div>
+        
+            <div>
+                <ul>
+                    <li>Piim</li>
+                    <li>Laua viin</li>
+                    <li>Õlu</li>
+                    <li>Sipsid</li>
+                </ul>  
+        </div>   	
+    <div>
+</template>
+
+<style>
+
+</style>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import './style.scss'
-console.log("hello world");
+import './style.scss';
+import { createApp } from 'vue';
+
+import App from './App.vue'
+const app = createApp(App)
+app.mount('#app');
