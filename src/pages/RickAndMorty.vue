74d5b274ffde585addc885aad8a38a3d9d937049
markojakob <markjakobo@gmail.com>
2025-03-18 11:12:17 +0200

    added debounce

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -7,34 +7,79 @@ import Pagination from '../components/Pagination.vue';
 const characters = ref([]);
 const info = ref({});
 const currentPage = ref(2);
+const searchvalue = ref('');
+const error = ref('');
+let searchTimeout = null;
+
 await getCharacters('https://rickandmortyapi.com/api/character');
 
 
 async function getCharacters(url) {
-    let response = await axios.get(url);
+    try {
+
+    
+    let response = await axios.get('https://rickandmortyapi.com/api/character', {
+        params: {
+            page: currentPage.value,
+            name: searchvalue.value,
+        }
+    });
     console.log(response.data);
     characters.value = response.data.results;
     info.value = response.data.info;
+    }
+    catch(err) {
+        console.log(err);
+        error.value = 'No results found';
+        characters.value = [];
+        info.value = null;
+    }
 }
 
 async function next() {
     currentPage.value++;
-    await getCharacters(info.value.next);
-    
+    await getCharacters();
+
 }
 
 async function prev() {
     currentPage.value--;
-    await getCharacters(info.value.prev);
+    await getCharacters();
+
+}
+
+async function page(page) {
+    currentPage.value = page;
+    await getCharacters();
+}
+
+async function search(){
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(async () => {
+        currentPage.value = 1;
+        error.value = '';
+        await getCharacters();
+    }, 1000);
 
 }
 </script>
 
 <template>
-    <Pagination :info="info" :curent="currentPage" @next="next" @prev="prev"></Pagination>
+    <div class="field has-addons">
+        <div class="control is-expanded">
+            <input @input="search" v-model="searchvalue" class="input" type="text" placeholder="Find a repository">
+        </div>
+        <div class="control">
+            <button @click="search" class="button is-info">
+                Search
+            </button>
+        </div>
+    </div>
+    <Pagination v-if="info" :info="info" :current="currentPage" @next="next" @prev="prev" @page="page"></Pagination>
     <div class="columns is-multiline">
         <div v-for="character in characters" class="column is-one-quarter">
             <CharacterCard :character="character"></CharacterCard>
         </div>
     </div>
+    <h1 v-if="error" class="is-size-1">{{ error }}</h1>
 </template>
